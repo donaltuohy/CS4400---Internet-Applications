a0013d9de00cd3ccb4e6c9e6693c673a51421990
donaltuohy <tuohydo@tcd.ie>
2017-11-14 14:21:42 +0000

    Adjusting for test server

--- Project_1/testingThreads.py
+++ Project_1/testingThreads.py
@@ -23,8 +23,8 @@ def deleteClient(listOfClients, name):
     del listOfClients[name]
 
 def parseName(joinMessage):
-    Username = joinMessage.split()[8]
-    chatroomName = joinMessage.split()[2]
+    Username = joinMessage.split()[7]
+    chatroomName = joinMessage.split()[1]
     return chatroomName, Username
 
 def parseMessage(chatMessage):
@@ -37,13 +37,13 @@ def parseMessage(chatMessage):
     return chatroom, joinID, clientName, message
 
 def HELO(host, port, message):
-    return  message + "\nIP: " + str(host) + "\nPort: " + str(port) + "\nStudentID: 14313774\n" 
+    return  message + "IP: " + str(host) + "\nPort: " + str(port) + "\nStudentID: 14313774\n" 
 
 def createChatBroadcast(roomID, clientName, message):
-    return "CHAT: " + str(roomID) + "\nCLIENT_NAME: " + clientName + "\nMESSAGE: " + message
+    return "CHAT: " + str(roomID) + "\nCLIENT_NAME: " + clientName + "MESSAGE: " + message
 
 def createJoinBroadcast(chatroomName, host, port, roomID, joinID):
-    return "JOINED_CHATROOM: " + chatroomName + "\nSERVER_IP: " + str(host) + "\nPORT: " + str(port) + "\nROOM_REF: " +  str(roomID) + "\nJOIN_ID: " + str(joinID)
+    return "JOINED_CHATROOM: " + chatroomName + "\nSERVER_IP: " + str(host) + "\nPORT: " + str(port) + "\nROOM_REF: " +  str(roomID) + "\nJOIN_ID: " + str(joinID) +"\n"
 
 #Function which broadcast a message to all connected clients bar the server and the one that sent the message
 def broadCastData(listOfClients,sock, message):
@@ -100,9 +100,9 @@ class ThreadedServer(object):
                 print(data)
                 #CLIENT SENDS JOIN MESSAGE
                 if(data[:4] == "HELO"):
-                    response = HELO(self.host, self.port, message).encode()
+                    response = HELO(self.host, self.port, data).encode()
                     client.send(response)
-                elif(data[:13] == "JOIN CHATROOM"):
+                elif(data[:13] == "JOIN_CHATROOM"):
                     chatroomName, clientName = parseName(data)
                     if  chatroomName not in listOfRooms:
                         listOfRooms[chatroomName] = [chatRoom(chatroomName,client, clientName, self.chatRoomIdCount)]
@@ -116,7 +116,7 @@ class ThreadedServer(object):
                     print(clientName, " has the JoinID: ", (listOfRooms[chatroomName])[0].clientIDs)
                     client.send((createJoinBroadcast(chatroomName, self.host, self.port, (listOfRooms[chatroomName])[0].ID, (listOfRooms[chatroomName])[0].clientIDs)).encode())
                     (listOfRooms[chatroomName])[0].clientIDs += 1
-                    joinchat = createChatBroadcast((listOfRooms[chatroomName])[0].roomID,clientName, "<" + clientName + "> has joined the room")
+                    joinchat = createChatBroadcast((listOfRooms[chatroomName])[0].ID,clientName, "<" + clientName + "> has joined the room")
                     broadCastData((listOfRooms[chatroomName])[0].listOfClients, client, joinchat)
                     client.send(joinchat.encode())
                     
